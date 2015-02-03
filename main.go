6ce894350e79b3fa80596003abd92d5de203785a
Johannes Müller <joh_m@gmx.de>
2015-02-03 08:13:54 +0100

    use templates, added stack view

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
-//import "fmt"
 import (
 	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/gen/cloudformation"
 	"github.com/awslabs/aws-sdk-go/gen/ec2"
 	"github.com/gin-gonic/gin"
-	//	"html/template"
 )
 
 var (
 	creds = aws.DetectCreds("", "", "")
 	cli   = ec2.New(creds, "eu-west-1", nil)
+	cfn   = cloudformation.New(creds, "eu-west-1", nil)
 )
 
+func resourcesByStackName(StackName string) []cloudformation.StackResource {
+	resp, err := cfn.DescribeStackResources(&cloudformation.DescribeStackResourcesInput{StackName: aws.String(StackName)})
+	if err != nil {
+		panic(err)
+	}
+	return resp.StackResources
+}
+
 func subnetsByVPCID(VPCID string) []ec2.Subnet {
-	resp, err := cli.DescribeSubnets(&ec2.DescribeSubnetsRequest{Filters: []ec2.Filter{ec2.Filter{aws.String("vpc-id"), []string{VPCID}}}})
+	resp, err := cli.DescribeSubnets(&ec2.DescribeSubnetsRequest{Filters: []ec2.Filter{
+		ec2.Filter{aws.String("vpc-id"), []string{VPCID}},
+	}})
 	if err != nil {
 		panic(err)
 	}
@@ -22,7 +32,9 @@ func subnetsByVPCID(VPCID string) []ec2.Subnet {
 }
 
 func instancesBySubnet(SubnetID string) []ec2.Instance {
-	resp, err := cli.DescribeInstances(&ec2.DescribeInstancesRequest{Filters: []ec2.Filter{ec2.Filter{aws.String("subnet-id"), []string{SubnetID}}}})
+	resp, err := cli.DescribeInstances(&ec2.DescribeInstancesRequest{Filters: []ec2.Filter{
+		ec2.Filter{aws.String("subnet-id"), []string{SubnetID}},
+	}})
 	if err != nil {
 		panic(err)
 	}
@@ -45,55 +57,118 @@ func tagByKey(tags []ec2.Tag, key string) string {
 	return ""
 }
 
-func viz(c *gin.Context) {
-	type VPC struct {
-		VPCID string
-		Name  string
-	}
-	var vpcList []VPC
-
-	type Subnet struct {
-		SubnetID string
-		Name     string
-	}
-	var subnetList []Subnet
+func vpc(c *gin.Context) {
 
 	type Instance struct {
 		InstanceID string
 		Name       string
 	}
-	var instanceList []Instance
+
+	type Subnet struct {
+		SubnetID  string
+		Name      string
+		Instances []Instance
+	}
+
+	type VPC struct {
+		VPCID   string
+		Name    string
+		Subnets []Subnet
+	}
+
+	var vpcList []VPC
 
 	resp, err := cli.DescribeVPCs(nil)
 	if err != nil {
 		panic(err)
 	}
 	for _, vpc := range resp.VPCs {
-		vpcList = append(vpcList, VPC{
+		tVPC := VPC{
 			VPCID: *vpc.VPCID,
-			Name:  tagByKey(vpc.Tags, "Name")})
+			Name:  tagByKey(vpc.Tags, "Name")}
 
 		subnets := subnetsByVPCID(*vpc.VPCID)
 		for _, subnet := range subnets {
-			subnetList = append(subnetList, Subnet{
+
+			tSubnet := Subnet{
 				SubnetID: *subnet.SubnetID,
-				Name:     tagByKey(subnet.Tags, "Name")})
+				Name:     tagByKey(subnet.Tags, "Name")}
 
 			instances := instancesBySubnet(*subnet.SubnetID)
 
 			for _, instance := range instances {
-				instanceList = append(instanceList, Instance{
+
+				tInstance := Instance{
 					InstanceID: *instance.InstanceID,
-					Name:       tagByKey(instance.Tags, "Name")})
+					Name:       tagByKey(instance.Tags, "Name")}
+
+				tSubnet.Instances = append(tSubnet.Instances, tInstance)
 
 			}
+
+			tVPC.Subnets = append(tVPC.Subnets, tSubnet)
 		}
+
+		vpcList = append(vpcList, tVPC)
 	}
 
-	obj := gin.H{"vpcs": vpcList,
-		"subnets":   subnetList,
-		"instances": instanceList}
-	c.HTML(200, "layout.tmpl", obj)
+	obj := gin.H{"VPCs": vpcList}
+	c.HTML(200, "layout_vpc.tmpl", obj)
+
+}
+
+func stack(c *gin.Context) {
+
+	type Instance struct {
+		InstanceID string
+		Name       string
+	}
+
+	type Subnet struct {
+		SubnetID  string
+		Name      string
+		Instances []Instance
+	}
+
+	type Resource struct {
+		Type               string
+		LogicalResourceID  string
+		PhysicalResourceID string
+	}
+
+	type Stack struct {
+		Name      string
+		Resources []Resource
+	}
+
+	var stackList []Stack
+
+	resp, err := cfn.DescribeStacks(nil)
+	if err != nil {
+		panic(err)
+	}
+	for _, stack := range resp.Stacks {
+		tStack := Stack{
+			Name: *stack.StackName,
+		}
+
+		resources := resourcesByStackName(*stack.StackName)
+		for _, resource := range resources {
+
+			tResource := Resource{
+				Type:               *resource.ResourceType,
+				LogicalResourceID:  *resource.LogicalResourceID,
+				PhysicalResourceID: *resource.PhysicalResourceID,
+			}
+
+			tStack.Resources = append(tStack.Resources, tResource)
+		}
+
+		stackList = append(stackList, tStack)
+	}
+
+	obj := gin.H{"Stacks": stackList}
+	c.HTML(200, "layout_stack.tmpl", obj)
 
 }
 
@@ -101,7 +176,8 @@ func main() {
 	// Creates a gin router + logger and recovery (crash-free) middlewares
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
-	r.GET("/viz", viz)
+	r.GET("/vpc", vpc)
+	r.GET("/stack", stack)
 	r.Static("/assets", "./assets")
 
 	// Listen and server on 0.0.0.0:8080
